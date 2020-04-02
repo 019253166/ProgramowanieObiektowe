23dece3f08b5b285571b0e5456aa48d88a79b3af
253166 <253166@student.pwr.edu.pl>
2020-04-02 21:44:06 +0200

    Dodanie argumentów do funkcji wyświetlającej tablicę

--- lab1/tablica.cpp
+++ lab1/tablica.cpp
@@ -2,15 +2,15 @@
 #include "tablica.h"
 using namespace	std;
 
-int* zmien_rozmiar(int* wsk_tablica, int x, int y){
-	return 0;
+void zmien_rozmiar(int* wsk_tablica, int x, int y){
+	
 }
 
 void zmien_zawartosc_komorki(int* wsk_tablica, int x, int y){
 	
 }
 
-void wyswietlanie_tablicy(int* wsk_tab){
+void wyswietlanie_tablicy(int* wsk_tab, int x, int y){
 	
 }
 
--- lab1/tablica.h
+++ lab1/tablica.h
@@ -1,3 +1,3 @@
-int* zmien_rozmiar(int* wsk_tablica, int x, int y);
+void zmien_rozmiar(int* wsk_tablica, int x, int y);
 void zmien_zawartosc_komorki(int* wsk_tablica, int x, int y);
-void wyswietlanie_tablicy(int* wsk_tab);
+void wyswietlanie_tablicy(int* wsk_tab, int x, int y);
